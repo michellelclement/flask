6cf7527be8a979822acf8a3ac5d8b9e35df014f9
Michelle Clement <michellelclement@gmail.com>
2020-08-11 14:56:07 +0000

    Finished personalising templates. Error to solve - missing nav

--- run.py
+++ run.py
@@ -8,13 +8,16 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route('/about')
 def about():
     return render_template("about.html")
 
+
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
 
 @app.route('/careers')
 def careers():
