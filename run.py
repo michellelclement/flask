2be61bd55de77c2ea64cf94d62560bbb513e91a6
Michelle Clement <michellelclement@gmail.com>
2020-08-17 08:07:12 +0000

    Adding form and flash messages to contact page

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import os
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = 'secret_name'
 
 
 @app.route('/')
@@ -30,8 +31,10 @@ def about_member(member_name):
 
     return render_template("member.html", member=member)
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(request.form["name"]))
     return render_template("contact.html", page_title="Contact")
 
 
